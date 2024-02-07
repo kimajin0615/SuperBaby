1c5fa017fdcb22b13dfb0ef3777e35fc1c8e7aef
kimajin0615 <120775596+kimajin0615@users.noreply.github.com>
2024-02-07 20:45:03 +0900

    Lua Test
    Lua Test

--- Assets/Resources/Dialogue/Test.lua
+++ Assets/Resources/Dialogue/Test.lua
@@ -1,8 +1,7 @@
+
+
 Test = CreateDialog(function()
-
     Talk("미친아진","테스트용 문장","TestImage")
-
+    Talk("이름"    ,    "대사"    ,    "이미지")
     local select = MakeSelect("미친아진","김수연 루아 시킬까?","TestImage",{"무슨소리! 무조건 시키자","당연히 시킨다"})
-
-
 end)
