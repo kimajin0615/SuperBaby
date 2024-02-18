51c3614ad899ca30e0d2e28c391b748f7793cac1
kimajin0615 <120775596+kimajin0615@users.noreply.github.com>
2024-02-18 15:55:15 +0900

    Backup
    Backup

---
+++ Assets/Scripts/Kim/DataManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    static GameObject container;
+
+    static DataManager instance;
+    public static DataManager Instance
+    {
+        get
+        {
+            if(!instance)
+            {
+                container = new GameObject();
+                container.name = "DataManager";
+                instance = container.AddComponent(typeof(DataManager)) as DataManager;
+                DontDestroyOnLoad(container);
+            }
+            return instance;
+        }
+    }
+
+    string GameDataFileName = "GameData.json";
+
+    
+}
