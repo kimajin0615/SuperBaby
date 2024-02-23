f88cd8710c157320315c322295deb9c6ebf242c1
kimajin0615 <120775596+kimajin0615@users.noreply.github.com>
2024-02-23 18:44:06 +0900

    game data
    game data

--- Assets/Scripts/Data/ItemI.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemObject", menuName = "ScriptableObject/ItemObject", order = 1)]
-public class ItemI : ScriptableObject
-{
-
-}
