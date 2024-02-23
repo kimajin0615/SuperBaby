010c046b1373a59a6e99905584cf8a3fece65216
KAH24Gjiha <128780283+KAH24Gjiha@users.noreply.github.com>
2024-02-23 10:09:38 +0900

    .
    .

--- Assets/Scripts/Hu/Mapmanager.cs
+++ Assets/Scripts/Hu/Mapmanager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Mapmanager : MonoBehaviour
 {
+    public static GameObject home;
+    public static GameObject shop;
+    public static GameObject area;
+    public static GameObject park;
     // Start is called before the first frame update
     void Start()
     {
-        
+        home = transform.GetChild(0).gameObject;
+        shop = transform.GetChild(1).gameObject;
+        area = transform.GetChild(2).gameObject;
+        park = transform.GetChild(3).gameObject;
     }
 
     // Update is called once per frame
