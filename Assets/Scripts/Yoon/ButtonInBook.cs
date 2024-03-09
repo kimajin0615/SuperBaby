ac0d628af23ceade3c31b775562b545da5099dc6
gaeZZang <128779897+gaeZZang@users.noreply.github.com>
2024-03-09 13:24:41 +0900

    phone

---
+++ Assets/Scripts/Yoon/ButtonInBook.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInBook: MonoBehaviour
+{
+    public GameObject objectToActivate;
+    public GameObject objectToDeactivate;
+
+    void Start()
+    {
+       
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+   
+        if (objectToActivate != null && !objectToActivate.activeSelf)
+        {
+            objectToActivate.SetActive(true);
+        }
+
+        if (objectToDeactivate != null && objectToDeactivate.activeSelf)
+        {
+            objectToDeactivate.SetActive(false); 
+        }
+    }
+}
---
+++ Assets/Scripts/Yoon/PhoneMove.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneMove : MonoBehaviour
+{
+    public Transform targetPosition;
+    public Transform targetPosition2;
+    public float moveSpeed = 2.0f;
+
+    private bool isMoving = false;
+    private bool isMovingBack = false;
+   
+    private Vector3 startPosition;
+    private float journeyLength;
+    public Button button;
+    private float startTime;
+
+   
+
+    void Start()
+    {
+        startPosition = transform.position;
+        journeyLength = Vector3.Distance(startPosition, targetPosition.position);
+
+        if (button != null)
+        {
+            button.onClick.AddListener(MoveObject);
+        }
+    }
+
+    void Update()
+    {
+       
+        
+        if (isMoving)
+        {
+            float distanceCovered = (Time.time - startTime) * moveSpeed;
+            float fractionOfJourney = distanceCovered / journeyLength;
+            transform.position = Vector3.Lerp(startPosition, targetPosition.position, fractionOfJourney);
+
+            if (fractionOfJourney >= 1.0f)
+            {
+                isMoving = false;
+            }
+            
+}
+
+        if (isMovingBack)
+        {
+            float distanceCovered = (Time.time - startTime) * moveSpeed;
+            float fractionOfJourney = distanceCovered / journeyLength;
+            transform.position = Vector3.Lerp(startPosition, targetPosition2.position, fractionOfJourney);
+
+            if (fractionOfJourney >= 1.0f)
+            {
+                isMovingBack = false;
+            }
+        }
+
+
+
+        
+    }
+
+    private void MoveObject()
+    {
+        isMoving = true;
+        startPosition = transform.position;
+        journeyLength = Vector3.Distance(startPosition, targetPosition.position);
+        startTime = Time.time;
+    }
+
+    public void MoveBack()
+    {
+        isMovingBack = true;
+        startPosition = transform.position;
+        journeyLength = Vector3.Distance(startPosition, targetPosition2.position);
+        startTime = Time.time;
+    }
+}
