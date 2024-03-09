a37219cb08baf3107756bd925fc2e02ebc66cd8b
gaeZZang <128779897+gaeZZang@users.noreply.github.com>
2024-03-09 17:32:21 +0900

    phone
    .

--- Assets/Resources/Dialogue/Testtest.lua
+++ Assets/Resources/Dialogue/Testtest.lua
@@ -1,8 +1,8 @@
 Testtest = CreateDialog(function()
 
-    Talk("다호이","천혜향 맛있다","testimage")
+    Talk("다호이","천혜향 맛있다","TestImage")
 
-    local select = MakeSelect("다호이","하나 더 먹을까","testimage",{"지금 밤이야","당연히 먹는다"})
+    local select = MakeSelect("다호이","하나 더 먹을까","testimage",{"지금 밤이야","당연히 먹는다", "무슨 소리야?"})
 
 
 end)
