3ab11d26bed737d073dc88a4395e956ae4f1f5ef
gaeZZang <128779897+gaeZZang@users.noreply.github.com>
2024-03-09 20:43:39 +0900

    .

--- Assets/Scripts/Yoon/BackButton.cs
+++ Assets/Scripts/Yoon/BackButton.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class BackButton : MonoBehaviour
 {
+    public UnityEngine.UI.Button BackButtonUI;
+    public UnityEngine.UI.Button HomeButton;
+
+
+    public GameObject ChatScreen;
+    public GameObject ChatList;
+    public GameObject ChatRoom;
+    public GameObject CalenderScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,22 @@ public class BackButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ChatScreen.activeSelf)
+        {
+            if (ChatList.activeSelf)
+            {
+
+            }
+
+            if (ChatRoom.activeSelf)
+            {
+
+            }
+        }
+
+        if (CalenderScreen.activeSelf)
+        {
+            HomeButton.onClick.Invoke();
+        }
     }
 }
